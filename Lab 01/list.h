5fe90d2bb6ea072ac00d55515deda83b4c94326f
Devki Nandan Sharma <d_nsharma@outlook.com>
2025-05-29 14:04:01 -0400

    updated list.c

--- Lab 01/list.h
+++ Lab 01/list.h
@@ -1,1 +1,14 @@
 #pragma once
+// Group 03 - tanveer - Devki - PROG72370 - S25
+
+struct PRODUCT {
+	int productID;
+	int productQty;
+	PRODUCT* next;
+};
+
+PRODUCT* CreateList(int n);
+PrintList(PRODUCT* head);
+TraverseList(PRODUCT* head);
+ReduceQty(PRODUCT* head);
+FreeList(PRODUCT* head);
