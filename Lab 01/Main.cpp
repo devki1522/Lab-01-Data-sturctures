7346825ae01fc0b84db634541e65ee5a990831d6
Devki Nandan Sharma <d_nsharma@outlook.com>
2025-05-29 14:03:08 -0400

    Finished main .cpp

--- Lab 01/Main.cpp
+++ Lab 01/Main.cpp
@@ -1,10 +1,32 @@
+
 // Group 03 - tanveer - Devki - PROG72370 - S25
 
 #include <iostream>
+#include "list.h"
 
 using namespace std;
 
 int main(void) {
+	int n;
+	cout << "enter a number to create list: ";
+	cin >> n;
+
+	PRODUCT* newlist = CreateList(n); // creating a new list with input from user 
+
+	cout << "\nPrinting list with original Quantity. \n";
+
+	PrintList(newlist);
+
+	ReduceQty(newlist); // this will go thru the list and find quanties greater than30 and reduce them by 5
+
+	cout << "\n\n\nFinal list after reduced Quantity.\n";
+
+	PrintList(newlist);	// print the updated list
+
+
+	FreeList(newlist);	// frees allocated memory
+
 
 	return 0;
 }
+
